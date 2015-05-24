ccd1137df0d9764ee1cb1bf1108c5f995599a51c
Robert Thackston <robert.thackston@gmail.com>
2015-05-23 20:23:09 -0400

    Initializing reddit exercise 165_hard

---
+++ DailyProgrammer/165_hard/cpp/src/forest.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+    // Initialize parameters
+    double lumberjackPercentage = 10/100;
+    double treePercentage = 20/100;
+    double bearPercentage = 5/100;
+    int mapSize = 10;
+}
